c2811be24351bf4e9e99d300226ec686d73a59dc
Quốc Bảo <53300523+baolhq@users.noreply.github.com>
2020-08-10 22:13:01 +0700

    Add files via upload

---
+++ wordsField.js
@@ -0,0 +1,53 @@
+const technologies = [
+  "hello-world",
+  "Linus-Torvalds",
+  "hackerman",
+  "programmer",
+  "preprocessor",
+  "information-technology",
+  "decompiler",
+  "firewall",
+  "back-tracking",
+  "make-it-comes-true",
+];
+
+const games = [
+  "pong",
+  "minesweeper",
+  "boomberman",
+  "leaguage-of-legends",
+  "dark-souls",
+  "mario-kart",
+  "bloody-roar",
+  "cyberpunk-2077",
+  "counter-strikes",
+  "grand-theft-auto",
+  "Teenage-Mutant-Ninja-Turtles",
+];
+
+const countries = [
+  "japan",
+  "vietnam",
+  "germany",
+  "cuba",
+  "australia",
+  "bangladesh",
+  "denmark",
+  "afghanistan",
+  "madagascar",
+  "repulic-of-korea",
+];
+
+const animes = [
+  "doraemon",
+  "naruto",
+  "your-name",
+  "dragon-ball",
+  "attack-on-titan",
+  "black-clever",
+  "death-note",
+  "goblin-slayer",
+  "overlord",
+  "fate-grand-order",
+  "assassination-classroom",
+];
